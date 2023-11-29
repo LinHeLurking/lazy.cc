1faff8dced9c8ead1ebcad14c00bdca4bf9d1e46
LinHeLurking <LinHe.Lurking@gmail.com>
2023-11-29 10:56:33 +0800

    bench: recover template bench;
    Signed-off-by: LinHeLurking <LinHe.Lurking@gmail.com>

--- benchmark/bench_unroll.cc
+++ benchmark/bench_unroll.cc
@@ -25,7 +25,7 @@ static void BM_UnrollLoop_Macro(benchmark::State& state) {
   auto dst = new int8_t[n];
 
   for (auto _ : state) {
-    std::fill(src, src + n, 0xBB);
+    std::fill(src, src + n, 0xAA);
     int8_t *p = src, *q = dst;
     M_Unroll4(
         n,
@@ -51,36 +51,36 @@ static void BM_UnrollLoop_Macro(benchmark::State& state) {
 }
 BENCHMARK(BM_UnrollLoop_Macro);
 
-//static void BM_UnrollLoop_Template(benchmark::State& state) {
-//  size_t n = 1000;
-//  auto src = new int8_t[n];
-//  auto dst = new int8_t[n];
-//
-//  for (auto _ : state) {
-//    std::fill(src, src + n, 0xCC);
-//    int8_t *p = src, *q = dst;
-//    _::Unroll(
-//        n,
-//        [&p, &q]() {
-//          *q = *p & int8_t(0x80);
-//          p++;
-//          q++;
-//        },
-//        [&p, &q]() {
-//          *((int16_t*)(q)) = *((int16_t*)p) & int16_t(0x80 << 8 | 0x80);
-//          p += 2;
-//          q += 2;
-//        },
-//        [&p, &q]() {
-//          *((int32_t*)q) = *((int32_t*)p) &
-//                           int32_t(0x80 << 24 | 0x80 << 16 | 0x80 << 8 | 0x80);
-//          p += 4;
-//          q += 4;
-//        });
-//  }
-//  delete[] src;
-//  delete[] dst;
-//}
-//BENCHMARK(BM_UnrollLoop_Template);
+static void BM_UnrollLoop_Template(benchmark::State& state) {
+  size_t n = 1000;
+  auto src = new int8_t[n];
+  auto dst = new int8_t[n];
+
+  for (auto _ : state) {
+    std::fill(src, src + n, 0xAA);
+    int8_t *p = src, *q = dst;
+    _::Unroll(
+        n,
+        [&p, &q]() {
+          *q = *p & int8_t(0x80);
+          p++;
+          q++;
+        },
+        [&p, &q]() {
+          *((int16_t*)(q)) = *((int16_t*)p) & int16_t(0x80 << 8 | 0x80);
+          p += 2;
+          q += 2;
+        },
+        [&p, &q]() {
+          *((int32_t*)q) = *((int32_t*)p) &
+                           int32_t(0x80 << 24 | 0x80 << 16 | 0x80 << 8 | 0x80);
+          p += 4;
+          q += 4;
+        });
+  }
+  delete[] src;
+  delete[] dst;
+}
+BENCHMARK(BM_UnrollLoop_Template);
 
 BENCHMARK_MAIN();
--- src/unroll.hpp
+++ src/unroll.hpp
@@ -2,36 +2,36 @@
 #define LAZY_EXAMPLE_UNROLL_HPP
 
 // Using lambda is slow :) There may be some failures when inlining.
-// #include "./force_inline.hpp"
-//
-// namespace _ {
-// template <class A1, class A2, class A4>
-// FORCE_INLINE void Unroll(size_t n, A1 &&a1, A2 &&a2, A4 &&a4) {
-//   size_t i = n >> 2;
-//   for (; i > 0; --i) {
-//     a4();
-//   }
-//   if (n & 2) {
-//     a2();
-//   }
-//   if (n & 1) {
-//     a1();
-//   }
-// }
-//
-// template <class A1>
-// FORCE_INLINE void Unroll(size_t n, A1 &&a1) {
-//   auto a2 = [&a1]() {
-//     a1();
-//     a1();
-//   };
-//   auto a4 = [&a2]() {
-//     a2();
-//     a2();
-//   };
-//   Unroll(n, a1, a2, a4);
-// }
-// }  // namespace _
+#include "./force_inline.hpp"
+
+namespace _ {
+template <class A1, class A2, class A4>
+FORCE_INLINE void Unroll(size_t n, A1 &&a1, A2 &&a2, A4 &&a4) {
+  size_t i = n >> 2;
+  for (; i > 0; --i) {
+    a4();
+  }
+  if (n & 2) {
+    a2();
+  }
+  if (n & 1) {
+    a1();
+  }
+}
+
+template <class A1>
+FORCE_INLINE void Unroll(size_t n, A1 &&a1) {
+  auto a2 = [&a1]() {
+    a1();
+    a1();
+  };
+  auto a4 = [&a2]() {
+    a2();
+    a2();
+  };
+  Unroll(n, a1, a2, a4);
+}
+}  // namespace _
 
 #define M_Unroll4(n, a1, a2, a4) \
   {                              \
