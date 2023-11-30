a96ab3a76d7b75415651977d92e4c9e4b57aa686
LinHeLurking <LinHe.Lurking@gmail.com>
2023-11-30 16:01:56 +0800

    bench: add int32 bench;
    Signed-off-by: LinHeLurking <LinHe.Lurking@gmail.com>

--- benchmark/bench_unroll.cc
+++ benchmark/bench_unroll.cc
@@ -4,7 +4,10 @@
 
 constexpr size_t n = 100000;
 
-static void BM_NaiveLoop(benchmark::State& state) {
+class Int8Unroll : public benchmark::Fixture {};
+class Int32Unroll : public benchmark::Fixture {};
+
+BENCHMARK_F(Int8Unroll, Naive)(benchmark::State& state) {
   auto src = new int8_t[n];
   auto dst = new int8_t[n];
 
@@ -18,9 +21,8 @@ static void BM_NaiveLoop(benchmark::State& state) {
   delete[] src;
   delete[] dst;
 }
-BENCHMARK(BM_NaiveLoop);
 
-static void BM_UnrollLoop_Macro(benchmark::State& state) {
+BENCHMARK_F(Int8Unroll, Macro4Way)(benchmark::State& state) {
   auto src = new int8_t[n];
   auto dst = new int8_t[n];
 
@@ -49,9 +51,8 @@ static void BM_UnrollLoop_Macro(benchmark::State& state) {
   delete[] src;
   delete[] dst;
 }
-BENCHMARK(BM_UnrollLoop_Macro);
 
-static void BM_UnrollLoop_Template(benchmark::State& state) {
+BENCHMARK_F(Int8Unroll, Template4Way)(benchmark::State& state) {
   auto src = new int8_t[n];
   auto dst = new int8_t[n];
 
@@ -80,9 +81,8 @@ static void BM_UnrollLoop_Template(benchmark::State& state) {
   delete[] src;
   delete[] dst;
 }
-BENCHMARK(BM_UnrollLoop_Template);
 
-static void BM_UnrollLoop_Template_Way1(benchmark::State& state) {
+BENCHMARK_F(Int8Unroll, Template1Way)(benchmark::State& state) {
   auto src = new int8_t[n];
   auto dst = new int8_t[n];
 
@@ -98,6 +98,119 @@ static void BM_UnrollLoop_Template_Way1(benchmark::State& state) {
   delete[] src;
   delete[] dst;
 }
-BENCHMARK(BM_UnrollLoop_Template_Way1);
+
+BENCHMARK_F(Int32Unroll, Naive)(benchmark::State& state) {
+  auto src = new int32_t[n];
+  auto dst = new int32_t[n];
+
+  for (auto _ : state) {
+    std::fill(src, src + n, 0xAA);
+    for (int i = 0; i < n; ++i) {
+      dst[i] = src[i] & int32_t(0x80);
+    }
+  }
+
+  delete[] src;
+  delete[] dst;
+}
+
+BENCHMARK_F(Int32Unroll, Macro4Way)(benchmark::State& state) {
+  auto src = new int32_t[n];
+  auto dst = new int32_t[n];
+
+  for (auto _ : state) {
+    std::fill(src, src + n, 0xAA);
+    int32_t *p = src, *q = dst;
+    M_Unroll4(
+        n,
+        {
+          *q = *p & int32_t(0x80);
+          p++;
+          q++;
+        },
+        {
+          *q = *p & int32_t(0x80);
+          p++;
+          q++;
+          *q = *p & int32_t(0x80);
+          p++;
+          q++;
+        },
+        {
+          *q = *p & int32_t(0x80);
+          p++;
+          q++;
+          *q = *p & int32_t(0x80);
+          p++;
+          q++;
+          *q = *p & int32_t(0x80);
+          p++;
+          q++;
+          *q = *p & int32_t(0x80);
+          p++;
+          q++;
+        });
+  }
+  delete[] src;
+  delete[] dst;
+}
+
+BENCHMARK_F(Int32Unroll, Temaplte4Way)(benchmark::State& state) {
+  auto src = new int32_t[n];
+  auto dst = new int32_t[n];
+
+  for (auto _ : state) {
+    std::fill(src, src + n, 0xAA);
+    int32_t *p = src, *q = dst;
+    _::Unroll(
+        n,
+        [&p, &q]() {
+          *q = *p & int32_t(0x80);
+          p++;
+          q++;
+        },
+        [&p, &q]() {
+          *q = *p & int32_t(0x80);
+          p++;
+          q++;
+          *q = *p & int32_t(0x80);
+          p++;
+          q++;
+        },
+        [&p, &q]() {
+          *q = *p & int32_t(0x80);
+          p++;
+          q++;
+          *q = *p & int32_t(0x80);
+          p++;
+          q++;
+          *q = *p & int32_t(0x80);
+          p++;
+          q++;
+          *q = *p & int32_t(0x80);
+          p++;
+          q++;
+        });
+  }
+  delete[] src;
+  delete[] dst;
+}
+
+BENCHMARK_F(Int32Unroll, Template1Way)(benchmark::State& state) {
+  auto src = new int32_t[n];
+  auto dst = new int32_t[n];
+
+  for (auto _ : state) {
+    std::fill(src, src + n, 0xAA);
+    int32_t *p = src, *q = dst;
+    _::Unroll(n, [&p, &q]() {
+      *q = *p & int32_t(0x80);
+      p++;
+      q++;
+    });
+  }
+  delete[] src;
+  delete[] dst;
+}
 
 BENCHMARK_MAIN();
